6dce10b84280a90d096484c4ab2d7413ae60f381
Achint Bhat <achint94@asu.edu>
2023-03-02 18:36:10 -0700

    add API calls

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,20 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/core";
 
 import formSchema from './form-schema.json';
+import user from './user.json'
+import { getAllResumes, createResume } from './resume';
+
+
+
+async function fetchResumes() {
+  try {
+    const data = await getAllResumes()
+    return data
+  } catch (error) {
+    console.error(error);
+    return [user]
+  }
+}
+
+
+
+
 
 
 function MyForm() {
-  function handleSubmit(formData) {
-    console.log('Form submitted:', formData);
+
+
+  const [formData, setFormData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchResumes()
+      setFormData(data[0])
+    }
+    fetchData()
+  }, []);
+
+  function handleSubmit(data) {
+    delete data.formData._id
+    createResume(data.formData)
   }
+  // function handleUpdate(formData) {
+  //   console.log('Form submitted for update:', formData);
+  // }
 
   return (
-    <Form schema={formSchema}
-      validator={validator}
-      onSubmit={handleSubmit}
-    />
+    <div style={{ margin: '2rem auto', maxWidth: 600 }}>
+
+      <Form schema={formSchema}
+        validator={validator}
+        onSubmit={handleSubmit}
+        formData={formData}
+      >
+        
+      </Form >
+    </div >
+
 
   );
 }
---
+++ resume/src/resume.js
@@ -0,0 +1,22 @@
+const BASE_URL = process.env.REACT_APP_API_BASE_URL
+
+export async function getAllResumes() {
+    const url = `${BASE_URL}/resume`;
+    const response = await fetch(url, { mode: 'cors' });
+    const data = await response.json();
+    return data;
+}
+
+export async function createResume(resumeData) {
+    console.log(resumeData)
+    const url = `${BASE_URL}/resume`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(resumeData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
